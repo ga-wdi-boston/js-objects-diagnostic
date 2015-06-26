bf71f2477824a487e5e2fc60cbdd5026c19355f7
MattB-GA-Boston <matt.brendzel@generalassemb.ly>
2015-06-25 23:36:54 -0400

    Cleans up spec file
    Makes the Jasmine code more idiomatic and elegant - replacing `.not.toBe(undefined)` with `.toBeDefined()`, for instance.

--- spec/objects.spec.js
+++ spec/objects.spec.js
@@ -7,36 +7,35 @@ console.log(challenge);
 describe('objects', function(){
   describe('answerTwo', function(){
     it('is not undefined',function(){
-      expect(challenge.answerTwo).not.toBe(undefined);
+      expect(challenge.answerTwo).toBeDefined();
     });
     it('has property "name", which is not null', function(){
-      expect(challenge.answerTwo.name).not.toBe(undefined);
-      expect(challenge.answerTwo.name).not.toBe(null);
+      expect(challenge.answerTwo.name).toBeDefined();
+      expect(challenge.answerTwo.name).not.toBeNull();
     });
     it('has property "address", which is not null', function(){
-      expect(challenge.answerTwo.address).not.toBe(undefined);
-      expect(challenge.answerTwo.address).not.toBe(null);
+      expect(challenge.answerTwo.address).toBeDefined();
+      expect(challenge.answerTwo.address).not.toBeNull();
     });
   });
   describe('answerThree', function(){
     it('is not undefined',function(){
-      expect(challenge.answerThree).not.toBe(undefined);
+      expect(challenge.answerThree).toBeDefined();
     });
     it('has property "name", which is not null', function(){
-      expect(challenge.answerThree.name).not.toBe(undefined);
-      expect(challenge.answerThree.name).not.toBe(null);
+      expect(challenge.answerThree.name).toBeDefined();
+      expect(challenge.answerThree.name).not.toBeNull();
     });
     it('has property "species", which is not null', function(){
-      expect(challenge.answerThree.species).not.toBe(undefined);
-      expect(challenge.answerThree.species).not.toBe(null);
+      expect(challenge.answerThree.species).toBeDefined();
+      expect(challenge.answerThree.species).not.toBeNull();
     });
     it('has property "breed", which is not null', function(){
-      expect(challenge.answerThree.breed).not.toBe(undefined);
-      expect(challenge.answerThree.breed).not.toBe(null);
+      expect(challenge.answerThree.breed).toBeDefined();
+      expect(challenge.answerThree.breed).not.toBeNull();
     });
     it('has method "makeNoise", which returns a string', function(){
-      expect(challenge.answerThree.makeNoise).not.toBe(undefined);
-      expect(typeof challenge.answerThree.makeNoise).toBe('function');
+      expect(challenge.answerThree.makeNoise).toBeDefined();
       expect(typeof challenge.answerThree.makeNoise()).toBe('string');
     });
   });
