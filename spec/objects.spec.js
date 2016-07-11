ee2baae595d6894661cb2379b1109b78274342d1
laurenfazah <laurenfazah@gmail.com>
2016-07-11 14:00:42 -0400

    Updated to mocha tests
    Package.json and grunt/ also updated from node-template

--- spec/objects.spec.js
+++ spec/objects.spec.js
@@ -1,42 +1,61 @@
 'use strict';
 
-var diagnostic = require('../lib/diagnostic.js');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+const diagnostic = require('../lib/diagnostic.js');
 
 console.log(diagnostic);
 
 describe('objects', function(){
   describe('answerTwo', function(){
     it('is not undefined',function(){
-      expect(diagnostic.answerTwo).toBeDefined();
+      return expect(diagnostic.answerTwo) !== undefined;
+    });
+    it('has property "name", which is defined', function(){
+      return expect(diagnostic.answerTwo.name) !== undefined;
     });
     it('has property "name", which is not null', function(){
-      expect(diagnostic.answerTwo.name).toBeDefined();
-      expect(diagnostic.answerTwo.name).not.toBeNull();
+      return expect(diagnostic.answerTwo.name) !== null;
+    });
+    it('has property "address", which is defined', function(){
+      return expect(diagnostic.answerTwo.address) !== undefined;
     });
     it('has property "address", which is not null', function(){
-      expect(diagnostic.answerTwo.address).toBeDefined();
-      expect(diagnostic.answerTwo.address).not.toBeNull();
+      return expect(diagnostic.answerTwo.address) !== null;
     });
   });
   describe('answerThree', function(){
     it('is not undefined',function(){
-      expect(diagnostic.answerThree).toBeDefined();
+      return expect(diagnostic.answerThree) !== undefined;
     });
     it('has property "name", which is not null', function(){
-      expect(diagnostic.answerThree.name).toBeDefined();
-      expect(diagnostic.answerThree.name).not.toBeNull();
+      return expect(diagnostic.answerThree.name) !== null;
+    });
+    it('has property "name", which is defined', function(){
+      return expect(diagnostic.answerThree.name) !== undefined;
     });
     it('has property "species", which is not null', function(){
-      expect(diagnostic.answerThree.species).toBeDefined();
-      expect(diagnostic.answerThree.species).not.toBeNull();
+      return expect(diagnostic.answerThree.species) !== null;
+    });
+    it('has property "species", which is defined', function(){
+      return expect(diagnostic.answerThree.species) !== undefined;
+    });
+    it('has property "breed", which is defined', function(){
+      return expect(diagnostic.answerThree.breed) !== undefined;
     });
     it('has property "breed", which is not null', function(){
-      expect(diagnostic.answerThree.breed).toBeDefined();
-      expect(diagnostic.answerThree.breed).not.toBeNull();
+      return expect(diagnostic.answerThree.breed) !== null;
+    });
+    it('has method "vocalize", which is defined', function(){
+      return expect(diagnostic.answerThree.vocalize) !== undefined;
     });
     it('has method "vocalize", which returns a string', function(){
-      expect(diagnostic.answerThree.vocalize).toBeDefined();
-      expect(typeof diagnostic.answerThree.vocalize()).toBe('string');
+      return expect(typeof diagnostic.answerThree.vocalize()) === 'string';
     });
   });
 });
