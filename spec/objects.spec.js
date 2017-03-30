80c8bb7e832a5eeb6d3faf5d18056f222191b9fe
Ben Adamski <bengitscode@gmail.com>
2017-03-30 12:46:44 -0400

    Make standard
    -  Ran grunt make-standard to standardize formatting and grunt nag
    runs clean.

--- spec/objects.spec.js
+++ spec/objects.spec.js
@@ -1,59 +1,59 @@
-'use strict';
+'use strict'
 
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 
-chai.use(chaiAsPromised);
+chai.use(chaiAsPromised)
 
-const expect = chai.expect;
+const expect = chai.expect
 
-const diagnostic = require('../lib/diagnostic.js');
+const diagnostic = require('../lib/diagnostic.js')
 
 describe('objects', function () {
   describe('answerTwo', function () {
     it('is not undefined', function () {
-      expect(diagnostic.answerTwo).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerTwo).to.not.equal(undefined)
+    })
     it('has property "name", which is defined', function () {
-      expect(diagnostic.answerTwo.name).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerTwo.name).to.not.equal(undefined)
+    })
     it('has property "name", which is not null', function () {
-      expect(diagnostic.answerTwo.name).to.not.equal(null);
-    });
+      expect(diagnostic.answerTwo.name).to.not.equal(null)
+    })
     it('has property "address", which is defined', function () {
-      expect(diagnostic.answerTwo.address).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerTwo.address).to.not.equal(undefined)
+    })
     it('has property "address", which is not null', function () {
-      expect(diagnostic.answerTwo.address).to.not.equal(null);
-    });
-  });
+      expect(diagnostic.answerTwo.address).to.not.equal(null)
+    })
+  })
   describe('answerThree', function () {
     it('is not undefined', function () {
-      expect(diagnostic.answerThree).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerThree).to.not.equal(undefined)
+    })
     it('has property "name", which is not null', function () {
-      expect(diagnostic.answerThree.name).to.not.equal(null);
-    });
+      expect(diagnostic.answerThree.name).to.not.equal(null)
+    })
     it('has property "name", which is defined', function () {
-      expect(diagnostic.answerThree.name).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerThree.name).to.not.equal(undefined)
+    })
     it('has property "species", which is not null', function () {
-      expect(diagnostic.answerThree.species).to.not.equal(null);
-    });
+      expect(diagnostic.answerThree.species).to.not.equal(null)
+    })
     it('has property "species", which is defined', function () {
-      expect(diagnostic.answerThree.species).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerThree.species).to.not.equal(undefined)
+    })
     it('has property "breed", which is defined', function () {
-      expect(diagnostic.answerThree.breed).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerThree.breed).to.not.equal(undefined)
+    })
     it('has property "breed", which is not null', function () {
-      expect(diagnostic.answerThree.breed).to.not.equal(null);
-    });
+      expect(diagnostic.answerThree.breed).to.not.equal(null)
+    })
     it('has method "vocalize", which is defined', function () {
-      expect(diagnostic.answerThree.vocalize).to.not.equal(undefined);
-    });
+      expect(diagnostic.answerThree.vocalize).to.not.equal(undefined)
+    })
     it('has method "vocalize", which returns a string', function () {
-      expect(typeof diagnostic.answerThree.vocalize()).to.equal('string');
-    });
-  });
-});
+      expect(typeof diagnostic.answerThree.vocalize()).to.equal('string')
+    })
+  })
+})
